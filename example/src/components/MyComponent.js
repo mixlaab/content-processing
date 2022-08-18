705cc8c5fc85033afdbbef3c86ab9c410bdea92c
Víctor Medrano <mixlaab.labs@gmail.com>
2022-08-18 01:37:55 +0000

    Add video processing feature

--- example/src/components/MyComponent.js
+++ example/src/components/MyComponent.js
@@ -2,41 +2,46 @@ import React from 'react'
 import { useState } from 'react'
 import { useOpenCv } from 'opencv-react'
 import './MyComponent.css'
+// import JWPlayer from '@jwplayer/jwplayer-react'
 
 const MyComponent = () => {
   const data = useOpenCv()
   const { loaded, cv } = useOpenCv()
-  const [imageStatus, setImageStatus] = useState(null)
+  const [videoStatus, setVideoStatus] = useState(null)
   const [grayScale, setGrayScale] = useState(true)
   const [src, setSrc] = useState(null)
   const [dst, setDst] = useState(null)
 
-  const onImageChange = (e) => {
+  const onVideoChange = (e) => {
     console.log('e >> ', e.target.files[0])
     if (e.target.files[0]) {
-      let imgElement = document.getElementById('imageSrc')
-      imgElement.src = URL.createObjectURL(e.target.files[0])
-      setImageStatus(imgElement)
+      let videoElement = document.getElementById('videoInput')
+      videoElement.src = URL.createObjectURL(e.target.files[0])
+      setVideoStatus(videoElement)
     }
   }
 
   const onGrayScaleChange = () => {
     setGrayScale(!grayScale)
-    if (imageStatus){
-        if (grayScale) {
-            let s = cv.imread(imageStatus)
-            let d = new cv.Mat()
-            // You can try more different parameters
-            setSrc(s)
-            setDst(d)
-            cv.cvtColor(s, d, cv.COLOR_RGBA2GRAY, 0)
-            cv.imshow('canvasOutput', d)
-          } else {
-            let s = cv.imread(imageStatus);
-            cv.imshow('canvasOutput', s);
-          }
+    if (grayScale) {
+      let video = document.getElementById('videoInput')
+      let src = new cv.Mat(video.height, video.width, cv.CV_8UC4)
+      let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1)
+      let cap = new cv.VideoCapture(video)
+      const FPS = 30
+
+      function processVideo() {
+        let begin = Date.now()
+        cap.read(src)
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY)
+        cv.imshow('canvasOutput', dst)
+        // schedule next one.
+        let delay = 1000 / FPS - (Date.now() - begin)
+        setTimeout(processVideo, delay)
+      }
+      // schedule first one.
+      setTimeout(processVideo, 0)
     }
-    
   }
 
   if (loaded) {
@@ -59,16 +64,31 @@ const MyComponent = () => {
                 type='file'
                 id='fileInput'
                 name='file'
-                onChange={(e) => onImageChange(e)}
+                onChange={(e) => onVideoChange(e)}
               />
             </div>
           </div>
           <div className='tile'>
-            <img id='imageSrc' alt='No Image' />
+            {/* <JWPlayer
+              file='https://videos-fms.jwpsrv.com/0_62fdb459_0xeca2a7a6df429546000e5027c13e702278f89aa9/content/conversions/k2M5E7f8/videos/CXz339Xh-17199156.mp4'
+              library='https://content-portal.jwplatform.com/libraries/MgYBn7ND.js'
+            /> */}
+            <video
+              id='videoInput'
+              crossorigin='anonymous'
+              src='https://videos-fms.jwpsrv.com/0_62fdb459_0xeca2a7a6df429546000e5027c13e702278f89aa9/content/conversions/k2M5E7f8/videos/CXz339Xh-17199156.mp4'
+              width='320'
+              height='240'
+              controls
+            />
             <div className='caption'>Input Image</div>
           </div>
           <div className='tile'>
             <canvas id='canvasOutput'></canvas>
+            {/* <JWPlayer
+              file='https://videos-fms.jwpsrv.com/0_62fdb459_0xeca2a7a6df429546000e5027c13e702278f89aa9/content/conversions/k2M5E7f8/videos/CXz339Xh-17199156.mp4'
+              library='https://content-portal.jwplatform.com/libraries/MgYBn7ND.js'
+            /> */}
             <div className='caption'>Output Image</div>
           </div>
         </div>
